6c9b931778678449660672a5a6906572870f82a8
vergilium <vergilium2@gmail.com>
2021-02-24 01:11:35 +0200

    initial commit

---
+++ MyBOT/Services/TelebotService.cs
@@ -0,0 +1,16 @@
+﻿using BotClient;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBOT.Services {
+	public static class TelebotServiceExtention {
+		public static void AddTelebotService(this IServiceCollection service) {
+			service.AddSingleton<BotSettings, BotSettings>();
+			service.AddSingleton<TelebotClient, TelebotClient>();
+		}
+	}
+}
---
+++ TelegramBotClient/BotSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BotClient {
+	/// <summary>
+	/// Bot settings class.
+	/// </summary>
+	public sealed class BotSettings {
+		/// <value>name of property in settings.json file</value>
+		public const string botOptions = "BotOptions";
+		/// <value> URL string for web hook</value>
+		public string url { get; set; }
+		///<value>Name of telegram bot</value>
+		public string name { get; set; }
+		///<value>Telegram API access key</value>
+		public string key { get; set; }
+
+	}
+}
